b202adaeb1ee4fe1a45a0b12166f8da311136d42
Uzoka Hillary <hillarybond10@gmail.com>
2025-11-12 00:30:53 +0100

    ready for deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,30 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
+
+
 import authRouter from './routes/auth.routes.js';
 import messageRouter from './routes/message.routes.js';
 
 dotenv.config();
 
 const app = express();
+const ___dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/v1/auth/", authRouter)
 app.use("api/v1/messages", messageRouter)
+
+// make ready for production
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(___dirname, '../frontend/dist')));
+
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(___dirname, '../frontend', 'dist', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log('Server is running on port ' + PORT);
