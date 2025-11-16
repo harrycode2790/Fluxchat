4fbd76d327f2acf12f1e54cf3842ee99d67aead6
Uzoka Hillary <hillarybond10@gmail.com>
2025-11-16 11:57:49 +0100

    made a change suggested

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,10 @@ import { Route, Routes } from "react-router";
 import ChatPage from "./pages/ChatPage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
-import { useAuthStore } from "./store/useAuthStore";
+
 
 function App() {
-  const {authUser, isLoading, login} = useAuthStore()
-
-  console.log("auth User ", authUser)
-  console.log("isloading" , isLoading)
-  login()
+  
  
   return (
     <div className="min-h-screen bg-gray-900 relative flex items-center justify-center p-4 overflow-hidden">
@@ -18,7 +14,7 @@ function App() {
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
       <div className="absolute top-0 -left-4 size-96 bg-brandColor opacity-20 blur-[100px]" />
       <div className="absolute bottom-0 -right-4 size-96 bg-brandColor opacity-20 blur-[100px]" />
-        <button></button>
+        
       <Routes>
         <Route path="/" element={<ChatPage />} />
         <Route path="/login" element={<LoginPage />} />
